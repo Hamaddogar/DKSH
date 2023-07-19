d51fa20250bcd43a09f69f96cc593242017be3ef
Hamad Dogar <42932321+Hamaddogar@users.noreply.github.com>
2023-07-19 15:15:54 +0500

    Update formRouter.js

--- server/Api/formRouter.js
+++ server/Api/formRouter.js
@@ -2,7 +2,7 @@ const Form = require('../Database/model/formModel');
 const UserSettings = require('../Database/model/settings');
 
 exports.saveFormData = async (req, res) => {
-  const { firstName, role, lastName, contactNo, country, email, password } = req.body;
+  const { firstName, role, lastName, contactNo, country, email, password, avatar } = req.body;
 
   try {
     const existingForm = await Form.findOne({ email });
@@ -11,7 +11,7 @@ exports.saveFormData = async (req, res) => {
       return res.send({ success: false, message: 'Email already exists' });
     }
 
-    const newForm = new Form({ firstName, role, lastName, contactNo, country, email, password, fullName: `${firstName} ${lastName}` });
+    const newForm = new Form({ avatar, firstName, role, lastName, contactNo, country, email, password, fullName: `${firstName} ${lastName}` });
 
     const userSettings = new UserSettings({ userId: newForm._id });
     newForm.settings = userSettings._id;
