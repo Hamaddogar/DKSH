cbb380e75401aa5f23300b9a3b821799a4dc2ec8
Muhammad Hamad <42932321+Hamaddogar@users.noreply.github.com>
2023-08-22 22:17:52 -0700

    Update endPoints.js

--- client/src/utils/endPoints.js
+++ client/src/utils/endPoints.js
@@ -1,14 +1,14 @@
 
-// const server = 'http://localhost:8080';
+const server = 'http://localhost:8080';
 
 
 export const APIS = {
-    all_dev: '/alluser',
-    all_jobs: '/jobss',
-    login: '/api/login',
-    signup: '/signup',
-    forgot: '/forgot',
-    resetpassword: '/resetpassword',
-    updateSettings: '/setting/update',
-    getCountry: '/country',
+    all_dev: server + '/alluser',
+    all_jobs: server + '/jobss',
+    login: server + '/api/login',
+    signup: server + '/signup',
+    forgot: server + '/forgot',
+    resetpassword: server + '/resetpassword',
+    updateSettings: server + '/setting/update',
+    getCountry: server + '/country',
 }
